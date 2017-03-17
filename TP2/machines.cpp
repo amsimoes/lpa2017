11ae7f2dbc7e5215281b335c92e6e9c75a01714e
Joao Luis Gaspar Lopes <jllopes@student.dei.uc.pt>
2017-03-17 11:40:55 +0000

    prob accepted

--- TP2/machines.cpp
+++ TP2/machines.cpp
@@ -7,25 +7,59 @@
 #include <math.h>
 #include <time.h>
 
-void readInput(){
-    int op_number;
-    double prob;
-    int cost;
-    int budget;
-    std::cin >> op_number;
-    //std::cout << "Operations number: " << op_number << "\n";
-    std::vector<std::pair<double,int> > machines; 
-    for(int i=0; i<op_number; i++){
-        std::cin >> prob >> cost;
-        machines.push_back(std::make_pair(prob,cost));
-        //std::cout << "Probability: " << machines.at(i).first << " " << "Cost: " << machines.at(i).second << "\n";
+std::vector<std::pair<double,int> > machines;
+double machines_prob[400];
+int machines_cost[400];
+int budget;
+int *values;
+
+
+int readInput(){
+    int sections; 
+    std::cin >> sections; 
+    //std::cout << "Number of sections: " << sections << "\n"; 
+    for(int i=0; i<sections; i++){
+        std::cin >> machines_prob[i] >> machines_cost[i]; 
+        //std::cout << "Probability: " << machines_prob[i] << " " << "Cost: " << machines_cost[i] << "\n";
     }
     std::cin >> budget;
     //std::cout << "Budget: " << budget << "\n";
+    return sections;
+}
+
+void knapsack(int sections, int budget){  
+    double machines_array[sections+1][budget+1];
+    double failure_prob;
+    double prob;
+    for(int i=0; i<=budget; i++){
+        machines_array[0][i] = 1;      
+    }  
+    for(int i=1; i<=sections; i++){ 
+        for(int j=1; j<=budget; j++){ 
+           machines_array[i][j] = 0;
+           int max = j/machines_cost[i-1];  
+           failure_prob = 1;
+           for(int k=1; k<=max; k++){
+              // (1-P)^n
+              failure_prob *= (1-machines_prob[i-1]);
+              // 1-(1-P)^n * Valor Anterior
+              prob = (1-failure_prob)*(machines_array[i-1][j-(k*machines_cost[i-1])]); 
+              if(prob > machines_array[i][j]){
+                  machines_array[i][j] = prob;
+              }
+           }   
+           //printf("i:%d, j:%d, %lf\n",i,j,machines_array[i][j]);
+        }
+    }
+    std::cout.precision(12);
+    std::cout << std::fixed << machines_array[sections][budget] << std::endl;
 }
 
 int main(void){
     std::ios::sync_with_stdio(false);
-    readInput();
+    int sections;
+    sections = readInput();
+    knapsack(sections,budget);
+    std::vector<std::pair<int, double> > machines_prob(sections, std::make_pair(0,0));
     return 0;
 }
