8b3135140a5da31e6c3b1275312958e7cbff645b
António Simões <asimoes@student.dei.uc.pt>
2017-05-15 22:45:50 +0100

    adds print transitions function

--- TP4/petri.cpp
+++ TP4/petri.cpp
@@ -28,15 +28,24 @@ node dominance - "x dominates y" if:
 
 */
 
-void dfs(int* places, trans_t** transitions, int n_places, int n_transitions, int level) {
+void print_transitions(trans_t** transitions, int n_places, int n_transitions) {
+	//printf("+++++++++++++++++++++++++++\n");
+	printf("###############################\n");
 	for(int i=0; i < n_transitions; i++) {
 		for(int j=0; j < n_places; j++) {
-			printf("transitions[%d]->from[%d] = %d\n", i, j, transitions[i]->from[j]);
+			printf("# transitions[%d]->from[%d] = %d #\n", i, j, transitions[i]->from[j]);
 		}
+		//printf("+++++++++++++++++++++++++++\n");
+		printf("# --------------------------- #\n");
 		for(int j=0; j < n_places; j++) {
-			printf("transitions[%d]->dest[%d] = %d\n", i, j, transitions[i]->dest[j]);
+			printf("# transitions[%d]->dest[%d] = %d #\n", i, j, transitions[i]->dest[j]);
 		}
+		printf("###############################\n");
 	}
+}
+
+void dfs(int* places, trans_t** transitions, int n_places, int n_transitions, int level) {
+	print_transitions(transitions, n_places, n_transitions);
 	//ver as transicoes que podem ser feitas, por ordem crescente ||||| se se puder fazer, alterar array de places, chamar a funcao recursiva |||| depois tirar a cena que foi alterada e continuar o for || se for 
 	for(int i=0; i < n_transitions; i++) {
 		int aux = 0;
@@ -75,7 +84,7 @@ void input(int &n_places, int &n_transitions, trans_t** &transitions, int* place
 
 	char line[64];
 	int n1, n2, type;
-	
+
 	while (scanf("%d %d %d", &n1, &n2, &type) == 3) {
 		if (type == 1) {	// Transição n1 vai para n2
 			transitions[n1-1]->dest[n2-1]++;
@@ -89,7 +98,7 @@ void input(int &n_places, int &n_transitions, trans_t** &transitions, int* place
 	fgets(line, 16, stdin);
 	for(int i=0; i<n_places; i++){
 		scanf("%d ",&places[i]);
-		printf("places[%d]: %d\n", i, places[i]);
+		//printf("places[%d]: %d\n", i, places[i]);
 	}
 }
 
