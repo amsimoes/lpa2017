298ee0f1dd1c23df084d18d8b3f4e4979e7c963a
António Simões <asimoes@student.dei.uc.pt>
2017-06-21 19:27:46 +0100

    floyd warshall commented

--- Prova2/floyd.cpp
+++ Prova2/floyd.cpp
@@ -17,13 +17,15 @@ int main()
 {
     ios::sync_with_stdio(false);
 
-    for (int i=0; i < n_cidades; i++) {
+    // Inicializar matrizes, no entanto so precisavamos de uma
+    for (int i=0; i < n_cidades; i++) { 
         for (int j=0; j < n_cidades; j++) {
             matrix[i][j] = 0;
             costs[i][j] = 0;
         }
     }
 
+    // INPUT
     scanf("%d", &n_cidades);
     for (int i=0; i < n_cidades; i++) {
         for (int j=0; j < n_cidades-1; j++) {
@@ -31,7 +33,7 @@ int main()
 
             scanf("%d ", &cost);
             if (cost == -1) {
-                matrix[i][j] = 99999;
+                matrix[i][j] = 99999;   // Usamos 99999 em vez de MAX_INT. Com MAX_INT o tipo da matriz seria long int para funcionar
             } else {
                 matrix[i][j] = cost;
             }
@@ -45,18 +47,22 @@ int main()
         }
     }
 
-    int s1, t1;
+    int s1, t1; // from, to
 
+    // Nao era preciso, bastava apenas usar a matrix.
     for (int i=0; i < n_cidades; i++) {
-            for (int j=0; j < n_cidades; j++) {
-                costs[i][j] = matrix[i][j];
-            }
+        for (int j=0; j < n_cidades; j++) {
+            costs[i][j] = matrix[i][j];
         }
+    }
 
+    // Floyd-Warshall: percorrer todas as combinações
     for (int k=0; k < n_cidades; k++) {
             for (int i=0; i < n_cidades; i++) {
                 for (int j=0; j < n_cidades; j++) {
-                    if (costs[i][j] > costs[i][k] + costs[k][j]) {
+                    // Se o caminho de i até k + de k até j que seja inferior que diretamente de i a j
+                    // Atualiza-se o valor
+                    if (costs[i][j] > costs[i][k] + costs[k][j]) {  
                         costs[i][j] = costs[i][k] + costs[k][j];
                     }
                 }
@@ -69,8 +75,6 @@ int main()
                 costs[i][j] = matrix[i][j];
             }
         }*/
-
-
 
         printf("%d\n", costs[s1-1][t1-1]);
     }
